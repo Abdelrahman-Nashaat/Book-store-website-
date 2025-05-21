0f2b366d8639be98bb183ebf719c89ec6814526e
Abdelrahman-Nashaat <emara01111@gmail.com>
2025-05-21 23:20:27 +0300

    Delete routes directory

--- routes/bookRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getBooks, getBookById, addBook } = require('../controllers/bookController');
-
-router.get('/', getBooks);
-router.get('/:id', getBookById);
-router.post('/', addBook);
-
-module.exports = router; 
